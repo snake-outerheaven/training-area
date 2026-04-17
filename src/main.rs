953b18eeeb142caffb0da4b7f151e47451e13623
neo-dev <sei133dirsoa@gmail.com>
2026-04-16 23:35:25 -0300

    Create guessing game of chapter 2 of the Rust Book.
    Co-authored-by: William Daudt Penna <Torist12@users.noreply.github.com>

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::{cmp::Ordering, io, process::Command};
+
+use rand::Rng;
+
+fn main() {
+    Command::new("clear").status().expect("Crash and Burn.");
+
+    println!("Guess the number!");
+
+    let secret = rand::thread_rng().gen_range(0..10);
+
+    println!("The secret number is {}.", secret);
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line"); // expect crasha o código inteiro, mostrando a msg
+
+        println!("You guessed: {guess}");
+
+        let num_input = guess
+            .trim()
+            .parse::<i32>()
+            .expect("Não foi possível converter"); // converte para um número a string digitada
+
+        match num_input.cmp(&secret) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
